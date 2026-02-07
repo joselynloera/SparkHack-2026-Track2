3bb36ab6d481a166b4abc47da6d990bc55b74ed8
lenarotteveel <lenarotteveel@gmail.com>
2026-02-07 17:16:20 -0600

    unfinal final

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ storyText.innerText =
     
     stress = Math.min(stress - 5, maxStress);
     updateStressBar();
-    localStorage.setItem("stress", stress);
+    sessionStorage.setItem("stress", stress);
     setTimeout(function(){
         window.location.href = "outside.html"
     }, 3000);
@@ -25,9 +25,38 @@ storyText.innerText =
     stress = Math.min(stress + 5, maxStress);
     updateStressBar();
 }
+
+if (choice === "doomscroll") {
+storyText.innerText =
+"You get on social media and flick through posts. Strangely, your efforts to relax are overshadowed by a lingering anxiety.";
+stress = Math.min(stress + 10, maxStress);
+    updateStressBar();
+    window.location.href = "homenight.html"
+    done();
+}
+if (choice === "sleep") {
+storyText.innerText =
+"You close your eyes and your body relaxes. You've earned this";
+stress = Math.min(stress - 5, maxStress);
+    updateStressBar();
+    window.location.href = "homenight.html"
+    done();
 }
 
-let stress = localStorage.getItem("stress");
+if (choice === "study") {
+storyText.innerText =
+"Your mind is jumbled from sleep depreivation. But hey, at least you're ahead of the game.";
+stress = Math.min(stress + 5, maxStress);
+    updateStressBar();
+    window.location.href = "homenight.html"
+    done();
+}
+}
+
+
+
+/////////////////////////////////////////////////////////////////////////////////
+let stress = sessionStorage.getItem("stress");
 
 if (stress === null) {
     stress = 20; // starting value
@@ -37,7 +66,7 @@ if (stress === null) {
 const maxStress = 100;
 let prepared = false;
 updateStressBar();
-localStorage.setItem("stress", stress);
+sessionStorage.setItem("stress", stress);
 const examButton = document.getElementById("examButton");
 const studyButton = document.getElementById("studyButton");
 const leaveButton = document.getElementById("leaveButton");
@@ -84,6 +113,9 @@ function updateStressBar() {
     bar.style.backgroundColor = "red";
   }
 }
+/////////////////////////////////////////////////////////////////////////////////
+
+
 
 function walk(){
     stress = Math.min(stress - 3, maxStress);
@@ -100,4 +132,56 @@ function school(){
     updateStressBar();
 }
 
+function goToNight(choice){
+    document.getElementById("game").style.display = "none";
+    document.getElementById("night").style.display = "block";
 
+    document.getElementById("time").innerText = "9:30 pm";
+
+    document.body.style.backgroundImage = "url('night_bedroom.jpg')";
+
+
+
+    if (choice === "doomscroll") {
+    storyText.innerText =
+    "You get on social media and flick through posts. Strangely, your efforts to relax are overshadowed by a lingering anxiety.";
+    stress = Math.min(stress + 10, maxStress);
+        updateStressBar();
+        window.location.href = "homenight.html"
+        done();
+    }
+    if (choice === "sleep") {
+    storyText.innerText =
+    "You close your eyes and your body relaxes. You've earned this";
+    stress = Math.min(stress - 5, maxStress);
+        updateStressBar();
+        window.location.href = "homenight.html"
+        done();
+    }
+
+    if (choice === "study") {
+    storyText.innerText =
+    "Your mind is jumbled from sleep depreivation. But hey, at least you're ahead of the game.";
+    stress = Math.min(stress + 5, maxStress);
+        updateStressBar();
+        window.location.href = "homenight.html"
+        done();
+    }
+}
+
+
+//leaving school
+function leave(){
+    window.location.href = "homenight.html"
+    goToNight();
+}
+
+
+function done(){
+    if(stress >= 50){
+        window.location.href = "shradha.html"
+    }
+    else {
+        window.location.href = "asma.html"
+    }
+}
