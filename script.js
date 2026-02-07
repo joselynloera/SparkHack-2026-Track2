08dd045af5e65fdb9dd58b2d1ade51c62092d244
lenarotteveel <lenarotteveel@gmail.com>
2026-02-07 16:20:57 -0600

    almost

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+
 function startGame(){
 document.getElementById("title-screen").style.display = "none";
 document.getElementById("game").style.display = "block";
@@ -10,48 +11,65 @@ const storyText = document.getElementById("story-text");
 if (choice === "up") {
 storyText.innerText =
 "You sit up, already tired. The day hasn’t even started yet. Maybe some fresh air would be nice.";
+    
+    stress = Math.min(stress - 5, maxStress);
+    updateStressBar();
+    localStorage.setItem("stress", stress);
     setTimeout(function(){
         window.location.href = "outside.html"
-    }, 5000);
-    stress = Math.min(stress - 20, maxStress);
-    updateStressBar();
+    }, 3000);
 }
 if (choice === "snooze") {
 storyText.innerText =
 "You close your eyes. Just five more minutes. Just this once.";
-    setTimeout(function(){
-        window.location.href = "home.html"
-    }, 5000);
-    stress = Math.min(stress - 20, maxStress);
+    stress = Math.min(stress + 5, maxStress);
     updateStressBar();
 }
 }
 
-let stress = 0;
+let stress = localStorage.getItem("stress");
+
+if (stress === null) {
+    stress = 20; // starting value
+} else {
+    stress = Number(stress);
+}
 const maxStress = 100;
 let prepared = false;
-let leave = false;
+updateStressBar();
+localStorage.setItem("stress", stress);
+const examButton = document.getElementById("examButton");
+const studyButton = document.getElementById("studyButton");
+const leaveButton = document.getElementById("leaveButton");
 
 
 //increase the stress
 function takeExam() {
     if (prepared){
         prepared = false;
+        document.getElementById("timer").textContent = "10:00 am";
+        examButton.disabled = true
     }
     else{
         stress = Math.min(stress + 20, maxStress);
+        document.getElementById("timer").textContent = "9:00 am";
+        examButton.disabled = true
+        studyButton.disabled = true
     }
     updateStressBar();
-
-
 }
 
 //decrease the stress
 function study() {
     prepared = true;
     stress = Math.max(stress - 15, 0);
+    document.getElementById("timer").textContent = "9:00 am";
+    studyButton.disabled = true;
     updateStressBar();
 }
+
+//leaving school
+
 
 //Updating the stress bad and addding colors on certain range
 function updateStressBar() {
@@ -68,11 +86,8 @@ function updateStressBar() {
 }
 
 function walk(){
-    stress = Math.min(stress - 20, maxStress);
+    stress = Math.min(stress - 3, maxStress);
     updateStressBar();
-    setTimeout(function(){
-        window.location.href = "school.html"
-    }, 5000);
 }
 
 function restart(){
@@ -81,4 +96,8 @@ function restart(){
 
 function school(){
     window.location.href = "school.html"
+    stress = Math.min(stress + 10, maxStress);
+    updateStressBar();
 }
+
+
